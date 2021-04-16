7a2eaa0910843f610a728105dc7ac3b67a115cbb
Guillermo Savero <gsq2081@gmail.com>
2021-04-15 23:33:58 -0500

    texto modificado

--- serve.js
+++ serve.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hola Mundo'))
+app.get('/', (req, res) => res.send('<h1>Hola Mundo</h1> <br> <p>Grupo6: Jacobo Frank, Savero Guillermo, Zevallos Araccelli</p>'))
 
 app.listen(3000);
 console.log('Server on port 3000');
